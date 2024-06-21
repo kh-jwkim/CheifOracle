02f464c4c582a716a4b1b96920bf5cc64ee2f45e
kh-jwkim <kh.jwkim@gmail.com>
2024-06-21 16:38:48 +0900

    [UPDATE] WORKBOOK 완료
    맡은 부분의 SQL 작성 완료함
    팀원들의 SQL이 완성되면 동작 여부 체크 필요

--- WorkBook/WorkBook.sql
+++ WorkBook/WorkBook.sql
@@ -119,18 +119,69 @@ DESC TB_CATEGORY;
 -- 아래 내용을 참고하여 적절한 SQL 문을 작성하시오.
 -- 뷰 이름(VW_학생일반정보), 컬럼(학번, 학생이름, 주소)
 
+-- CHUN 계정에 VIEW 생성 권한 설정(SYSTEM계정으로 수행)
+GRANT CREATE VIEW TO CHUN;
+
+-- 뷰 생성(CHUN계정으로 수행)
+CREATE VIEW VW_학생일반정보 (학번, 학생이름, 주소)
+AS SELECT STUDENT_NO, STUDENT_NAME, STUDENT_ADDRESS FROM TB_STUDENT;
+
+SELECT * FROM VW_학생일반정보;
+
 
 -- DDL 11
+-- 춘 기술대학교는 1년에 두 번씩 학과별로 학생과 지도교수가 지도 면담을 진행한다.
+-- 이를 위해 사용할 학생이름, 학과이름, 담당교수이름 으로 구성되어 있는 VIEW를 만드시오.
+-- 이때 지도 교수가 없는 학생이 있을 수 있음을 고려하시오
+-- (단, 이 VIEW 는 단순 SELECT만을 할 경우 학과별로 정렬되어 화면에 보여지게 만드시오.)
+
+CREATE VIEW COUNSLE_VIEW (학생이름, 학과이름, 담당교수이름)
+AS SELECT STUDENT_NAME, DEPARTMENT_NAME, PROFESSOR_NAME 
+FROM TB_STUDENT
+LEFT OUTER JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+LEFT OUTER JOIN TB_PROFESSOR ON COACH_PROFESSOR_NO = PROFESSOR_NO
+ORDER BY DEPARTMENT_NAME ASC;
+
+-- 학과별로 정렬되어있는지 확인
+SELECT * FROM COUNSLE_VIEW;
 
 
 -- DDL 12
+-- 모든 학과의 학과별 학생 수를 확인할 수 있도록 적절한 VIEW를 작성해 보자.
+-- 뷰 이름(VW_학과별학생수), 컬럼(DEPARTMENT_NAME, STUDENT_COUNT)
+
+CREATE VIEW VW_학과별학생수 (DEPARTMENT_NAME, STUDENT_COUNT)
+AS SELECT DEPARTMENT_NAME, COUNT(*)
+FROM TB_STUDENT
+JOIN TB_DEPARTMENT USING(DEPARTMENT_NO)
+GROUP BY DEPARTMENT_NAME
+ORDER BY 1 ASC;
+
+SELECT * FROM VW_학과별학생수;
 
 
 -- DML 3
+-- 국어국문학과 학생들의 정보만이 포함되어 있는 학과정보 테이블을 만들고자 한다.
+-- 아래 내용을 참고하여 적절한 SQL 문을 작성하시오.
+-- (힌트 : 방법은 다양함, 소신껏 작성하시오)
+-- 테이블이름(TB_국어국문학과), 컬럼(학번, 학생이름, 출생년도(4자리년도), 교수이름)
 
+CREATE TABLE TB_국어국문학과 (학번, 학생이름, 출생년도, 교수이름)
+AS SELECT STUDENT_NO, STUDENT_NAME, 
+DECODE(SUBSTR(STUDENT_SSN,8,1), '1', '19', '2', '19', '3', '20', '4', '20')||SUBSTR(STUDENT_SSN,1,2),
+PROFESSOR_NAME
+FROM TB_STUDENT
+LEFT OUTER JOIN TB_PROFESSOR ON COACH_PROFESSOR_NO = PROFESSOR_NO;
+
+-- 작성된 테이블 확인
+SELECT * FROM TB_국어국문학과;
 
 -- DML 4
+-- 현 학과들의 정원을 10% 증가시키게 되었다. 이에 사용할 SQL문을 작성하시오.
+-- (단, 반올림을 사용하여 소수점 자릿수는 생기지 않도록 한다)
 
+SELECT CAPACITY, DEPARTMENT_NAME FROM TB_DEPARTMENT ORDER BY DEPARTMENT_NAME ASC;
+UPDATE TB_DEPARTMENT E SET CAPACITY = ROUND(CAPACITY*1.1);
 
 
 
